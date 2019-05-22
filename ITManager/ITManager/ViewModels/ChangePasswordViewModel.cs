46cadc19f7967f16c2a489da3d06b6b549be5b7c
AlexeyRusakovich <alexrusakovich@mail.ru>
2019-05-22 14:45:42 +0300

    Added validation on change password view.

--- ITManager/ITManager/ViewModels/ChangePasswordViewModel.cs
+++ ITManager/ITManager/ViewModels/ChangePasswordViewModel.cs
@@ -28,7 +28,8 @@ namespace ITManager.ViewModels
 
         public ICommand GoToLoginView { get; set; }
 
-        public ChangePasswordViewModel(INavigationService navigationService, IEventAggregator eventAggregator) : base("Change Password")
+        public ChangePasswordViewModel(INavigationService navigationService, IEventAggregator eventAggregator) : base(
+            "Change Password")
         {
             ChangePassword = new DelegateCommand(ChangePasswordMethod);
             GoToLoginView = new DelegateCommand(GoToLoginViewMethod);
@@ -38,38 +39,55 @@ namespace ITManager.ViewModels
 
         private async void ChangePasswordMethod()
         {
-            using (var _database = new ITManagerEntities())
-            { 
-                var user = ShellViewModel.CurrentUser;
-                if (user != null)
+            if (IsValid(ValidatesProperties, out var errors))
+            {
+                if (NewPassword == ConfirmNewPassword)
                 {
-                    var _user = _database.Users.Where(u => u.Id == user.Id).FirstOrDefault();
-                    if (_user != null)
+                    Errors = string.Empty;
+                    using (var _database = new ITManagerEntities())
                     {
-                        var salt = Convert.FromBase64String(user.Salt);
-                        _user.Salt = Convert.ToBase64String(salt);
-                        _user.IsInitial = false;
-                        _user.Password = Convert.ToBase64String(PasswordHasher.ComputeHash(NewPassword, salt));
-                        _user.DefaultPassword = null;
-                        await _database.SaveChangesAsync();
+                        var user = ShellViewModel.CurrentUser;
+                        if (user != null)
+                        {
+                            var _user = _database.Users.Where(u => u.Id == user.Id).FirstOrDefault();
+                            if (_user != null)
+                            {
+                                var salt = Convert.FromBase64String(user.Salt);
+                                _user.Salt = Convert.ToBase64String(salt);
+                                _user.IsInitial = false;
+                                _user.Password = Convert.ToBase64String(PasswordHasher.ComputeHash(NewPassword, salt));
+                                _user.DefaultPassword = null;
+                                await _database.SaveChangesAsync();
 
-                        if (user.UserRoles.Any(r => r.RoleId == Constants.AdministratorRole))
-                        {
-                            _navigationService.NavigateTo(Constants.RolesManagementView);
-                        }
-                        else if (user.UserRoles.Any(r => r.RoleId == Constants.ManagerRole))
-                        {
-                            _navigationService.NavigateTo(Constants.SearchView);
-                        }
-                        else if(user.UserRoles.Any(r => r.RoleId == Constants.UserRole))
-                        {
-                            _navigationService.NavigateTo(Constants.MyPersonalPageView);
-                        }
+                                if (user.UserRoles.Any(r => r.RoleId == Constants.AdministratorRole))
+                                {
+                                    _navigationService.NavigateTo(Constants.RolesManagementView);
+                                }
+                                else if (user.UserRoles.Any(r => r.RoleId == Constants.ManagerRole))
+                                {
+                                    _navigationService.NavigateTo(Constants.SearchView);
+                                }
+                                else if (user.UserRoles.Any(r => r.RoleId == Constants.UserRole))
+                                {
+                                    _navigationService.NavigateTo(Constants.MyPersonalPageView);
+                                }
 
-                        _eventAggregator.GetEvent<CloseMenuEvent>().Publish(false);
+                                _eventAggregator.GetEvent<CloseMenuEvent>().Publish(false);
+                            }
+                        }
                     }
                 }
+                else
+                {
+                    Errors = "Passwords must match.";
+                }
             }
+            else
+            {
+                Errors = errors;
+            }
+
+            Errors = Errors.Trim();
         }
 
         private void GoToLoginViewMethod()
@@ -78,5 +96,40 @@ namespace ITManager.ViewModels
             parameters.Add("IsFromChangingPassword", true);
             _navigationService.NavigateToWithParameters(Constants.LoginView, navigationParameters: parameters);
         }
+
+        #region Data validation
+
+        public readonly string[] ValidatesProperties =
+        {
+            nameof(NewPassword),
+            nameof(ConfirmNewPassword)
+        };
+
+        public override string Validate(string propertyName)
+        {
+            if (!DoValidation)
+                return null;
+
+            switch (propertyName)
+            {
+                case nameof(NewPassword):
+                    if (NewPassword.IsNullOrWhiteSpace())
+                        return string.Format(Constants.FieldMustBeFilledMessageFormat, nameof(NewPassword));
+                    else if (!NewPassword.IsLengthBetween(5, 32))
+                        return string.Format(Constants.LengthErrorMessageFormat, nameof(NewPassword), 5, 32);
+                    break;
+
+                case nameof(ConfirmNewPassword):
+                    if (ConfirmNewPassword.IsNullOrWhiteSpace())
+                        return string.Format(Constants.FieldMustBeFilledMessageFormat, nameof(ConfirmNewPassword));
+                    else if (!ConfirmNewPassword.IsLengthBetween(5, 32))
+                        return string.Format(Constants.LengthErrorMessageFormat, nameof(ConfirmNewPassword), 5, 32);
+                    break;
+            }
+
+            return null;
+        }
+
+        #endregion
     }
 }
